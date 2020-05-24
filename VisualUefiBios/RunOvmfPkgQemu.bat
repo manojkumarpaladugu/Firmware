a8576c4500f696ec9b6883a8c454ec4458272429
manojkumarpaladugu <35074754+manojkumarpaladugu@users.noreply.github.com>
2020-05-24 19:46:38 +0530

    Updated RunOvmfPkgQemu.bat

---
+++ VisualUefiBios/RunOvmfPkgQemu.bat
@@ -0,0 +1,33 @@
+@echo off
+
+set ARCHITECTURE=%1
+set CONFIGURATION=%2
+
+if not defined QEMU (
+  echo QEMU not found. Please install QEMU and set envrionment variable under System Variable category
+  exit /b 1
+)
+
+if defined VS160COMNTOOLS (
+  set TOOL_SET=VS2019
+) else if defined VS150COMNTOOLS (
+  set TOOL_SET=VS2017
+) else if defined VS140COMNTOOLS (
+  set TOOL_SET=VS2015x86
+) else if defined VS120COMNTOOLS (
+  set TOOL_SET=VS2013x86
+) else (
+  echo "Error: Build\Ovmf%ARCHITECTURE%\%CONFIGURATION%_%TOOL_SET%\FV\OVMF.fd may not be available"
+  exit /b 1
+)
+
+cd ..
+
+if not exist %cd%/logs (
+  mkdir logs
+)
+if %CONFIGURATION% == DEBUG (
+  "%QEMU%\qemu-system-x86_64" -debugcon file:logs/OvmfPkg_%ARCHITECTURE%_%CONFIGURATION%.log -global isa-debugcon.iobase=0x402 -net none -bios Build\Ovmf%ARCHITECTURE%\%CONFIGURATION%_%TOOL_SET%\FV\OVMF.fd
+) else (
+  "%QEMU%\qemu-system-x86_64" -net none -bios Build\Ovmf%ARCHITECTURE%\%CONFIGURATION%_%TOOL_SET%\FV\OVMF.fd
+)
