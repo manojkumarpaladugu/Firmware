eea7de46256f75ccc382d9961d97c5a699b1d2ba
manojkumarpaladugu <35074754+manojkumarpaladugu@users.noreply.github.com>
2020-05-26 20:24:30 +0530

    Add files via upload

---
+++ BuildOvmfPkg.bat
@@ -0,0 +1,35 @@
+@echo off
+
+if defined VS160COMNTOOLS (
+  call "%VS160COMNTOOLS%\VsDevCmd.bat"
+  set TOOL_SET=VS2019
+) else if defined VS150COMNTOOLS (
+  call "%VS150COMNTOOLS%\VsDevCmd.bat"
+  set TOOL_SET=VS2017
+) else if defined VS140COMNTOOLS (
+  call "%VS140COMNTOOLS%VsDevCmd.bat"
+  set TOOL_SET=VS2015x86
+) else if defined VS120COMNTOOLS (
+  call "%VS120COMNTOOLS%VsDevCmd.bat"
+  set TOOL_SET=VS2013x86
+) else (
+  echo Error: Compiler not available, exiting now...
+  exit /b 1
+)
+
+set ARCHITECTURE=%1
+set CONFIGURATION=%2
+
+set WORKSPACE=%cd%
+set PACKAGES_PATH=%WORKSPACE%\edk2
+set NASM_PREFIX=C:\nasm\
+
+cd edk2
+if exist %cd%\BaseTools.Ready (
+  call edksetup.bat
+) else (
+  echo off > BaseTools.Ready
+  call edksetup.bat Rebuild
+)
+
+build -p OvmfPkg/OvmfPkg%ARCHITECTURE%.dsc -a %ARCHITECTURE% -b %CONFIGURATION% -t %TOOL_SET%
---
+++ RunOvmfPkgQemu.bat
@@ -0,0 +1,35 @@
+@echo off
+
+set ARCHITECTURE=%1
+set CONFIGURATION=%2
+
+if not defined QEMU (
+  echo Error: QEMU not found. Please install QEMU and set envrionment variable under System Variable category
+  exit /b 1
+)
+
+if defined VS160COMNTOOLS (
+  set TOOL_SET=VS2019
+) else if defined VS150COMNTOOLS (
+  set TOOL_SET=VS2017
+) else if defined VS140COMNTOOLS (
+  set TOOL_SET=VS2015x86
+) else if defined VS120COMNTOOLS (
+  set TOOL_SET=VS2013x86
+) else (
+  echo Error: Build\Ovmf%ARCHITECTURE%\%CONFIGURATION%_%TOOL_SET%\FV\OVMF.fd may not be available
+  exit /b 1
+)
+
+if not exist %cd%/logs (
+  mkdir logs
+)
+
+if %CONFIGURATION% == DEBUG (
+  "%QEMU%\qemu-system-x86_64" -debugcon file:logs/OvmfPkg_%ARCHITECTURE%_%CONFIGURATION%.log -global isa-debugcon.iobase=0x402 -net none -bios Build\Ovmf%ARCHITECTURE%\%CONFIGURATION%_%TOOL_SET%\FV\OVMF.fd
+) else if %CONFIGURATION% == RELEASE (
+  "%QEMU%\qemu-system-x86_64" -net none -bios Build\Ovmf%ARCHITECTURE%\%CONFIGURATION%_%TOOL_SET%\FV\OVMF.fd
+) else (
+  echo Error: Invalid configuration
+  exit /b 1
+)
