98979c7e8141528621498eb37765c0f205fc8cc4
manojkumarpaladugu <35074754+manojkumarpaladugu@users.noreply.github.com>
2020-06-04 11:05:29 +0530

    Update RunOvmfPkgQemu.bat

--- RunOvmfPkgQemu.bat
+++ RunOvmfPkgQemu.bat
@@ -26,12 +26,12 @@ if not exist %cd%/logs (
 )
 
 if "%CONFIGURATION%" == "DEBUG" (
-  "%QEMU%\qemu-system-x86_64" -debugcon file:logs/OvmfPkg_%ARCHITECTURE%_%CONFIGURATION%.log -global isa-debugcon.iobase=0x402 -net none -bios Build\Ovmf%ARCHITECTURE%\%CONFIGURATION%_%TOOL_SET%\FV\OVMF.fd
+  "%QEMU%\qemu-system-x86_64" -drive file=fat:rw:Build\OvmfX64\%CONFIGURATION%_%ARCHITECTURE%\X64,format=raw -debugcon file:logs/OvmfPkg_%ARCHITECTURE%_%CONFIGURATION%.log -global isa-debugcon.iobase=0x402 -net none -bios Build\Ovmf%ARCHITECTURE%\%CONFIGURATION%_%TOOL_SET%\FV\OVMF.fd
   exit /b 0
 )
 
 if "%CONFIGURATION%" == "RELEASE" (
-  "%QEMU%\qemu-system-x86_64" -net none -bios Build\Ovmf%ARCHITECTURE%\%CONFIGURATION%_%TOOL_SET%\FV\OVMF.fd
+  "%QEMU%\qemu-system-x86_64" -drive file=fat:rw:Build\OvmfX64\%CONFIGURATION%_%ARCHITECTURE%\X64,format=raw -net none -bios Build\Ovmf%ARCHITECTURE%\%CONFIGURATION%_%TOOL_SET%\FV\OVMF.fd
   exit /b 0
 )
 
